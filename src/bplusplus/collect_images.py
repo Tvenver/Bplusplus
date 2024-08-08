aed0ff5d2333a0d7c8f45ca49db5f4e4ed207c4d
Deniz Aydemir <deniz@aydemir.us>
2024-08-08 19:06:29 +0200

    full flow working through api

--- src/bplusplus/collect_images.py
+++ src/bplusplus/collect_images.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import random
 
 import pandas as pd
 import pygbif
@@ -10,85 +11,39 @@ class Occurrence:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
-# def collect_images(scientificNames: list[str], images_per_species: int, multimedia_file: str, output_directory: str):
-#     pass
+#TODO add back support for fetching from dataset (or csvs)
+def collect_images(scientificNames: list[str], images_per_species: int, output_directory: str):
+    __create_folders(
+        names=scientificNames,
+        directory=output_directory
+    )
 
-#Step 0: create folders to store images inside, names are based on a .csv file where each row contains one specie name
+    print("Beginning to collect images from GBIF...")
+    for name in scientificNames:
+        print(f"Collecting images for {name}")
+        occurrences = _fetch_occurrences(scientificName=name, totalLimit=10000)
+        print(f"{name} : {len(occurrences)} occurrences fetched, will sample for {images_per_species}")
 
-def collect_images(names_file: str, occurrence_file: str, multimedia_file:str, output_directory: str):
-    # Provide the path to your CSV file and the folder where you want to create subfolders
+        #TODO: filter for images then sample
 
-    print("collect images start")
+        random.seed(42) # for reproducibility
+        sampled_occurrences = random.sample(occurrences, min(images_per_species, len(occurrences)))
+        
+        for occurrence in sampled_occurrences:
+            image_url: str = occurrence.media[0].get("identifier")
+            if image_url is not None:
+                image_url = image_url.replace("original", "large") # hack to get max 1024px image
 
-    __create_folders_from_csv(names_file, output_directory)
-
-    # Step 1: Filtering the occurence dataset to only include species of interest, download images afterwards
-    #set variables
-    batch_size = 100000
-
-    
-
-    #specifiy path to occurence.txt file
-    csv_reader = pd.read_table(occurrence_file, chunksize=batch_size)
-    folders = __folders_in_directory(output_directory)
-    col = ['gbifID', 'species']
-    final_df = pd.DataFrame()  # Initialize the final DataFrame
-
-    occurrences: dict[str, list[Occurrence]] = {}
-
-
-    for name in folders:
-        occurrences[name] = _start_fetching_species(scientificName=name, totalLimit=15)
-        print(f"{name} : {len(occurrences[name])} occurrences")
-        print(f"{occurrences[name][0].media[0]["identifier"]}")
-
-    # Iterate through the batches
-    for batch_df in csv_reader:
-        batch_df = batch_df[col]
-        batch_df = batch_df[batch_df['species'].isin(folders)]
-        # Example: Print the first few rows of each batch to double check
-        print(batch_df.head())
-        final_df = pd.concat([final_df, batch_df], ignore_index=True)
-
-    #output final df to csv, where csv contains link to multimedia and species
-    final_df.to_csv(os.path.join(output_directory, 'occurence_filtered.csv'), index=False)
-
-    #Step 1.b.: Load in links to multimedia, leftjoin with filtered occurence data
-    #df1 = pd.read_table(os.path.join('data','multimedia.txt'), chunksize=batch_size)
-    df1 = pd.read_table(multimedia_file, chunksize=batch_size)
-    df2 = final_df
-
-    # Step 3: Iterate over the chunks and left join them
-    final_df = pd.DataFrame()  # Initialize the final DataFrame
-
-    for chunk in df1:
-        # Perform left join on the chunks
-        joined_chunk = __left_join_chunk(chunk, df2, 'gbifID')
-        filtered_df = joined_chunk[joined_chunk['species'].isin(folders)]
-        print(filtered_df)
-
-        # Append the joined chunk to the final DataFrame
-        final_df = pd.concat([final_df, filtered_df], ignore_index=True)
-
-    # Step 4: Save the final DataFrame to a new file or use it as needed
-    final_df.to_csv(os.path.join(output_directory, 'filtered_insect_species.csv'), index=False)
-
-    # Step 1.c.: Download images in new folders named according to your convention
-    df = final_df
-    # df = pd.read_csv('directory/to/csv/from/observ.org/photos/sampled_super_small.csv')
-    df['ID_name'] = df.index + 1
-
-    # Assuming df is your DataFrame
-    print('Start sampling per group')
-    sampled = df.groupby('species').apply(__sample_minimum).reset_index(drop=True)
-    sampled.to_csv(os.path.join(output_directory, 'sampled_super_small.csv'), index=False)
-    df = sampled
-
-    df.apply(lambda row: __down_image(row['identifier'], row['species'], row['ID_name']), axis=1)
+                __down_image(
+                    url=image_url,
+                    species=name,
+                    ID_name=occurrence.key
+                )
+            else:
+                print(f"Image URL not found for {occurrence.key}...")
 
 
-
-def _start_fetching_species(scientificName: str, totalLimit: int) -> list[Occurrence]:
+def _fetch_occurrences(scientificName: str, totalLimit: int) -> list[Occurrence]:
     return __next_batch_for_species(
         scientificName=scientificName,
         totalLimit=totalLimit,
@@ -98,15 +53,13 @@ def _start_fetching_species(scientificName: str, totalLimit: int) -> list[Occurr
 
 def __next_batch_for_species(scientificName: str, totalLimit: int, offset: int, current: list[Occurrence]) -> list[Occurrence]:
         search = pygbif.occurrences.search(scientificName=scientificName, limit=totalLimit, offset=offset, mediaType=["StillImage"])
-        print(search)
-        print(len(current))
+        results = search["results"]
+        occurrences = list(map(lambda x: Occurrence(**x), results))
         if search["endOfRecords"] or len(current) >= totalLimit:
-            return current
+            return current + occurrences
         else:
             offset = search["offset"]
-            results = search["results"]
             count = search["limit"] # this seems to be returning the count, and `count` appears to be returning the total number of results returned by the search
-            occurrences = list(map(lambda x: Occurrence(**x), results))
             return __next_batch_for_species(
                 scientificName=scientificName,
                 totalLimit=totalLimit,
@@ -114,18 +67,8 @@ def __next_batch_for_species(scientificName: str, totalLimit: int, offset: int, 
                 current=current + occurrences
             )
 
-# Step 1: Define a function to perform the left join on a specific chunk
-def __left_join_chunk(chunk1, chunk2, key_column):
-    return pd.merge(chunk1, chunk2, on=key_column, how='left')
-
-#uncomment sampling function, to reduce the test size to 150 image minimum or XXX% of original included testset (in our case from 60k images to 12k images)
-#df = pd.read_csv('directory/to/csv/from/observ.org/photos/sampled_super_small.csv')
-def __sample_minimum(group):
-    # Sample a minimum of 150 images or the total number of images if less than 150
-    return group.sample(n=min(150, len(group)), random_state=42)  # Added random_state for reproducibility
-
 #function to download insect images
-def __down_image(url, species, ID_name):
+def __down_image(url: str, species: str, ID_name: str):
     directory = os.path.join('data/dataset', f"{species}")
     os.makedirs(directory, exist_ok=True)
     image_response = requests.get(url)
@@ -133,26 +76,16 @@ def __down_image(url, species, ID_name):
     image_path = os.path.join(directory, image_name)
     with open(image_path, "wb") as f:
         f.write(image_response.content)
-    print(f"{species}{ID_name} downloaded successfully.")
+    print(f"{image_name} downloaded successfully.")
 
-
-def __folders_in_directory(directory: str) -> list[str]:
-    return [f for f in os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
-
-#create folders using first column of csv
-def __create_folders_from_csv(csv_file, directory_path):
-    print("create folders")
+def __create_folders(names: list[str], directory: str):
+    print("Creating folders for images...")
     # Check if the folder path exists, if not, create it
-    if not os.path.exists(directory_path):
-        os.makedirs(directory_path)
-    # Open the CSV file and read the names
-    with open(csv_file, 'r') as file:
-        csv_reader = csv.reader(file)
-        next(csv_reader)  # Skip the header
-        for row in csv_reader:
-            name = row[0].strip()  # Assuming the name is in the first column
-            print(name)
-            folder_name = os.path.join(directory_path, name)
-            # Create a folder with the name from the CSV
-            os.makedirs(folder_name, exist_ok=True)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    for name in names:
+        folder_name = os.path.join(directory, name)
+        # Create a folder using the scientific name
+        os.makedirs(folder_name, exist_ok=True)
 
--- src/bplusplus/train_validate.py
+++ src/bplusplus/train_validate.py
@@ -5,59 +5,68 @@ import shutil
 from ultralytics import YOLO
 
 
-def train_validate(data_directory: str, dataset_path: str, train_path: str, val_path: str):
+def train_validate(scientificNames: list[str], dataset_path: str, outputDirectory: str):
+# def train_validate(data_directory: str, dataset_path: str, train_path: str, val_path: str):
     # Define the ratio for splitting the dataset
     split_ratio = 0.8  # 80% for training, 20% for validation
+
+    train_path = os.path.join(outputDirectory, 'train')  # Path to the training folder
+    val_path = os.path.join(outputDirectory, 'val')   # Path to the validation folder
+
 
     # Create training and validation directories if they don't exist
     os.makedirs(train_path, exist_ok=True)
     os.makedirs(val_path, exist_ok=True)
 
     # Walk through the dataset directory
-    for root, dirs, files in os.walk(dataset_path):
-        for label in dirs:
-            label_path = os.path.join(root, label)
-            images = [f for f in os.listdir(label_path) if os.path.isfile(os.path.join(label_path, f))]
+    # for root, dirs, files in os.walk(dataset_path):
+    for species in scientificNames:
+        dataset_folder = os.path.join(dataset_path, species)
+        images = __files_in_folder(folder=dataset_folder)
 
-            # Shuffle the images
-            random.shuffle(images)
+        # Shuffle the images
+        random.shuffle(images)
 
-            # Calculate the split index
-            split_index = int(len(images) * split_ratio)
+        # Calculate the split index
+        split_index = int(len(images) * split_ratio)
 
-            # Split the images into training and validation sets
-            train_images = images[:split_index]
-            val_images = images[split_index:]
+        # Split the images into training and validation sets
+        train_images = images[:split_index]
+        val_images = images[split_index:]
 
-            # Create destination folders if they don't exist
-            train_label_path = os.path.join(train_path, label)
-            val_label_path = os.path.join(val_path, label)
-            os.makedirs(train_label_path, exist_ok=True)
-            os.makedirs(val_label_path, exist_ok=True)
+        # Create destination folders if they don't exist
+        train_label_path = os.path.join(train_path, species)
+        val_label_path = os.path.join(val_path, species)
+        os.makedirs(train_label_path, exist_ok=True)
+        os.makedirs(val_label_path, exist_ok=True)
 
-            # Move images to the appropriate folders
-            for image in train_images:
-                src = os.path.join(label_path, image)
-                dst = os.path.join(train_label_path, image)
-                shutil.move(src, dst)
+        # Move images to the appropriate folders
+        for image in train_images:
+            src = os.path.join(dataset_folder, image)
+            dst = os.path.join(train_label_path, image)
+            shutil.move(src, dst)
 
-            for image in val_images:
-                src = os.path.join(label_path, image)
-                dst = os.path.join(val_label_path, image)
-                shutil.move(src, dst)
+        for image in val_images:
+            src = os.path.join(dataset_folder, image)
+            dst = os.path.join(val_label_path, image)
+            shutil.move(src, dst)
 
     print("Dataset splitting completed successfully.")
 
     # Create a new YOLO model from scratch
-    model = YOLO('yolov8n-cls.pt')
+    model = YOLO(os.path.join(outputDirectory,'yolov8n-cls.pt'))
     #
     #define parameters for YOLO training, be aware of epoch, batch, and imgsz, to not exceed system requirements (memory, CPU, GPU...)
     #Folder for training *bplusplus/data/train
     #Folder for validation *bplusplus/data/val
     #Specify path to folder where the val and train folder is located
-    data = data_directory
+    data = outputDirectory
     results = model.train(data=data, epochs=5, batch=16, imgsz=224)
 
     #batch is adjusted to 1 to prevent a resizing bug - in training this bug doesnt emerge. A work around for larger batch size could be a resizing step in advance.
     model.val(batch=1)
 
+
+
+def __files_in_folder(folder: str) -> list[str]:
+    return [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
