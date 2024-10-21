e1f5b96a432b057af1b3d84d56ba09ed99bd1e57
orlandocloss <orlandocloss1@gmail.com>
2024-10-21 12:23:06 +0200

    prepared package (small error) 2

--- src/bplusplus/__init__.py
+++ src/bplusplus/__init__.py
@@ -2,3 +2,5 @@ from .build_model import build_model
 from .collect_images import Group, collect_images
 from .train_validate import train_validate
 from .collect_and_prepare import collect_and_prepare
+from src.yolov5detect.detect import run
+
