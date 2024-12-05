626d783a221443561bc41d3bf7a8d5752f05ad5a
orlandocloss <orlandocloss1@gmail.com>
2024-12-05 16:10:57 +0100

    loading bar, separating prepare script

--- src/bplusplus/__init__.py
+++ src/bplusplus/__init__.py
@@ -2,5 +2,6 @@ from .build_model import build_model
 from .collect_images import Group, collect_images
 from .train_validate import train_validate
 from .collect_and_prepare import collect_and_prepare
+from .prepare import prepare
 from .yolov5detect.detect import run
 
