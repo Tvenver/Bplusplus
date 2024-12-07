c74a98a30b0a2bb56938781e2b1d89fbccb4bf34
orlandocloss <orlandocloss1@gmail.com>
2024-12-07 19:49:16 +0100

    final pipeline

--- src/bplusplus/__init__.py
+++ src/bplusplus/__init__.py
@@ -4,4 +4,5 @@ from .train_validate import train_validate
 from .collect_and_prepare import collect_and_prepare
 from .prepare import prepare
 from .yolov5detect.detect import run
+from .train import train, validate
 
